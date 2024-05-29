20939d11b07abdd1a8e096eb8226bf13f6b421b9
Nyfeu <andre.maiolini@hotmail.com>
2024-05-28 21:41:20 -0300

    Criação Authenticator e DateParser.

---
+++ src/utils/DateParser.java
@@ -0,0 +1,16 @@
+package utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateParser {
+
+    public static Date parseString(String date) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.parse(date);
+
+    }
+
+}
