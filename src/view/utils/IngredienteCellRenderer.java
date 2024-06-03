45916e09cfe97ca8cba834fd2531fd2b4e5a4572
Nyfeu <andre.maiolini@hotmail.com>
2024-06-02 22:36:05 -0300

    Rastreio de ingredientes vencidos.

--- src/view/utils/IngredienteCellRenderer.java
+++ src/view/utils/IngredienteCellRenderer.java
@@ -1,25 +1,26 @@
 package view.utils;
 
 import model.entities.Ingrediente;
+import model.utils.DateParser;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class IngredienteCellRenderer extends JPanel implements ListCellRenderer<Ingrediente> {
 
     private JLabel lblNome;
     private JLabel lblQuantidade;
     private JLabel lblValidade;
-    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     public IngredienteCellRenderer() {
         setLayout(new BorderLayout(5, 5));
         lblNome = new JLabel();
         lblQuantidade = new JLabel();
         lblValidade = new JLabel();
-
         JPanel textPanel = new JPanel(new GridLayout(0, 1));
         textPanel.add(lblNome);
         textPanel.add(lblQuantidade);
@@ -32,7 +33,16 @@ public class IngredienteCellRenderer extends JPanel implements ListCellRenderer<
     public Component getListCellRendererComponent(JList<? extends Ingrediente> list, Ingrediente value, int index, boolean isSelected, boolean cellHasFocus) {
         lblNome.setText("Nome: " + value.getNome());
         lblQuantidade.setText("Quantidade: " + value.getQuantidade());
-        lblValidade.setText("Validade: " + dateFormat.format(value.getValidade()));
+
+        Date dataValidade = value.getValidade();
+
+        if (!vencido(dataValidade)) {
+            lblValidade.setText("Validade: " + DateParser.parseDate(dataValidade));
+            lblValidade.setForeground(Color.black);
+        } else {
+            lblValidade.setText("Validade: " + DateParser.parseDate(dataValidade) + " (VENCIDO)");
+            lblValidade.setForeground(Color.red);
+        }
 
         if (isSelected) {
             setBackground(list.getSelectionBackground());
@@ -45,6 +55,15 @@ public class IngredienteCellRenderer extends JPanel implements ListCellRenderer<
         setMaximumSize(new Dimension(list.getWidth(), getPreferredSize().height));
 
         return this;
+    }
+
+    private boolean vencido(Date dataValidade) {
+
+        LocalDate now = LocalDate.now();
+        Date utilDate = new Date(dataValidade.getTime());
+        LocalDate data = utilDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        return data.isBefore(now);
     }
 
 }
